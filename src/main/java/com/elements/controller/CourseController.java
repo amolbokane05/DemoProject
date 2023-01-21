d0d2091474d0787dbf1d98a08222278928e5862f
amolbokane05 <amolbokane05@gmail.com>
2023-01-21 22:15:35 +0530

    file added

---
+++ src/main/java/com/elements/controller/CourseController.java
@@ -0,0 +1,44 @@
+package com.elements.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CourseController {
+
+	
+	//course?name=SpringBoot
+			
+	@GetMapping("/course")
+	public String course(String name, Model model) {
+		
+		String mess = "Hi "+ name + " Welcome to JDC-04";
+		
+		model.addAttribute("msg", mess);
+		
+		return "index";
+	}
+	
+	//courseDetails?courseName=SpringBoot&trainerName=Anil
+	
+	@GetMapping("courseDetails")
+	public String courseDetails(@RequestParam String courseName,@RequestParam("tName") String trainerName, Model mod) {
+		 
+		String ms;
+		
+		if(courseName.equals("SpringBoot") && trainerName.equals("Charan")) {
+			
+			ms = courseName + " by " + trainerName + " starting from 17-Jan-2022 !!!";
+		}else {
+			ms = "Please contact to branch..!";
+		}
+		
+		mod.addAttribute("msg1", ms);
+		
+		return "index";
+	}
+	
+	
+}
---
+++ src/main/webapp/view/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Course Page</title>
+</head>
+<body>
+	<h1>${msg1}</h1>
+</body>
+</html>
